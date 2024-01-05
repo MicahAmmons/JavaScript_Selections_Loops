26c8e60ad02f55897ccf74fde1cf74ec04f1c659
Micah Ammons <micahammons@gmail.com>
2024-01-05 12:09:58 -0500

    Finished

--- app.js
+++ app.js
@@ -1,7 +1,90 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+for (let i = 0; i < 100; i++){
+    if (i % 2 == 0){
+        continue;
+    }else {
+        console.log(i);
+    }
+}
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+for (let i = 0; i < 100; i++){
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log("FIZZBUZZ");
+    }
+    else if (i % 3 == 0){
+        console.log("FIZZ");
+    }
+    else if (i % 5 == 0){
+        console.log("BUZZ");
+    } else {
+        console.log(i);
+    }
+}
+// Excercise 3
+{let i = 1;
+while (i <= 100) {
+    if (i % 2 == 0){
+        continue;
+    }else {
+        console.log(i);
+    }
+    i++;
+}}
+
+{let i = 1;
+do {
+    if (i % 2 == 0){
+        continue;
+    }else {
+        console.log(i);
+    }
+    i++
+} while (i <= 100);
+}
+
+{let i = 1;
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log("FIZZBUZZ");
+    }
+    else if (i % 3 == 0){
+        console.log("FIZZ");
+    }
+    else if (i % 5 == 0){
+        console.log("BUZZ");
+    } else {
+        console.log(i);
+    }}
+    
+    {let i = 1;
+    do {
+        if (i % 3 == 0 && i % 5 == 0){
+            console.log("FIZZBUZZ");
+        }
+        else if (i % 3 == 0){
+            console.log("FIZZ");
+        }
+        else if (i % 5 == 0){
+            console.log("BUZZ");
+        } else {
+            console.log(i);
+        } 
+    }  while (i <= 100)
+}
+
+// Excercise 4 
+let value = Math.round((Math.random() * 500)); // creates a random number between 0 and 500
+let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
+let found = false;
+for (i = 0; i <= n; i++){
+    if (i == value){
+        console.log("Found value!")
+        found = true;
+       break;
+    } 
+} 
+if (!found) {
+    console.log("Did not find value.");
+}
